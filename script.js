82c267a255824f485a9ff06f170b73037b4b9822
Mauro Lane <85325658+MauroLn26@users.noreply.github.com>
2023-09-15 08:55:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var audio = document.getElementById('som')
+   function aparecer(){
+   document.getElementById('inicio').style.border = '1px solid red';
+   var audio = new Audio('som');
+   audio.play();
+}
+
+
+
